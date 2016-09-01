e9f620398a95cb18fb096d0bd9820e287c13a91e
Marisha Sahay <marishasahay@gmail.com>
2016-08-31 21:30:18 -0700

    Add validation for not allow bad words

--- app/assets/javascripts/example_ctrl.js
+++ app/assets/javascripts/example_ctrl.js
@@ -5,13 +5,18 @@
     $scope.reviews = ["It was awesome","i love it","its confusing"];
 
     $scope.addReview = function(newReview) {
-      if(newReview != null) {
+      // if(newReview != null) {
+        if(newReview){
       $scope.reviews.push(newReview);
       $scope.newReview = null;
     }
     }
     $scope.taskComplete = function(index) {
       $scope.reviews.splice(index,1);
+    }
+
+    $scope.isPositive = function(task) {
+      return task.indexOf("stupid") === -1;
     }
     window.$scope = $scope;
 
